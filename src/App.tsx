bd2394e61a6ba90196d385cd738e46d4f19d345e
Hans-Christian Otto <c.otto@suora.com>
2022-11-02 14:33:50 +0100

    Added first component

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+
+function App() {
+  return <h1>Hallo ich bin Nikita</h1>;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
+const element = (
+  <div>
+    <App />
+    hallo aus react
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render(<h4>Hallo aus react 2</h4>, document.getElementById("root2"));
