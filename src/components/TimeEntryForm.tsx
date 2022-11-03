bd37ebfac8b81f8708821f7effbf2f28938076fc
Hans-Christian Otto <c.otto@suora.com>
2022-11-03 10:21:57 +0100

    🦄 Controlled component with weird behaviour

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -4,7 +4,11 @@ const TimeEntryForm = () => {
   const [inputValue, setInputValue] = useState("");
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setInputValue(event.target.value.toUpperCase());
+    if(event.target.value.length > 3) {
+      setInputValue(event.target.value.substring(1));
+      return;
+    }
+    setInputValue(event.target.value.toUpperCase().replace('A', 'B'));
   };
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
